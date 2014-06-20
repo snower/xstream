23743d75c06a78cd4dd1a48ca05a3d4f31fffa09
snower <sujin190@163.com>
2014-06-20 21:07:12 +0800

    fix crypto

--- xstream/crypto.py
+++ xstream/crypto.py
@@ -1,8 +1,24 @@
-# -*- coding: utf-8 -*-
-#14-6-20
+# _*_ coding: utf_8 _*_
+#14_6_20
 # create by: snower
 
 from M2Crypto import Rand,EVP
+
+ALG_KEY_IV_LEN = {
+    'aes_128_cfb': (16, 16),
+    'aes_192_cfb': (24, 16),
+    'aes_256_cfb': (32, 16),
+    'bf_cfb': (16, 8),
+    'camellia_128_cfb': (16, 16),
+    'camellia_192_cfb': (24, 16),
+    'camellia_256_cfb': (32, 16),
+    'cast5_cfb': (16, 8),
+    'des_cfb': (8, 8),
+    'idea_cfb': (16, 8),
+    'rc2_cfb': (8, 8),
+    'rc4': (16, 0),
+    'seed_cfb': (16, 16),
+}
 
 class Crypto(object):
     def __init__(self,key,alg='aes_256_cfb'):
@@ -11,12 +27,12 @@ class Crypto(object):
 
     def init_encrypt(self):
         self._ensecret=(self.rand_string(32),self.rand_string(32))
-        self._encipher=EVP.Cipher(self._alg,self._key,self._ensecret[0],1,1,'sha1',self._ensecret[1],1)
+        self._encipher=EVP.Cipher(self._alg,self.bytes_to_key(self._ensecret[0]),self._ensecret[1],1,0)
         return  "".join(self._ensecret)
 
     def init_decrypt(self,secret):
         self._desecret=(secret[:32],secret[32:])
-        self._decipher=EVP.Cipher(self._alg,self._key,self._desecret[0],0,1,'sha1',self._desecret[1],1)
+        self._decipher=EVP.Cipher(self._alg,self.bytes_to_key(self._ensecret[0]),self._desecret[1],0,0)
 
     def encrypt(self,data):
         return self._encipher.update(data)
@@ -26,3 +42,13 @@ class Crypto(object):
 
     def rand_string(self,length):
         return Rand.rand_bytes(length)
+    
+    def bytes_to_key(self,salt):
+        key_len=ALG_KEY_IV_LEN.get(self._alg)[0]
+        s=EVP.MessageDigest('sha1')
+        d1,d2=self._key,''
+        for i in range(5):
+            s.update(d1+salt)
+            d2=d1
+            d1=s.digest()
+        return (d1+d2)[:key_len]
