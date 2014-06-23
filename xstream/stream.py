0ef7b74c412f230317965405e82ac7271706dbd3
snower <sujin190@163.com>
2014-06-23 11:27:16 +0800

    fix stream ack

--- xstream/stream.py
+++ xstream/stream.py
@@ -179,7 +179,7 @@ class Stream(BaseStream):
             if now-self._last_data_time>=2 and self._frames and now-self._last_ack_time>=2:
                 self.write_control(SYN_ACK,struct.pack("!QB",self._current_frame_id,1))
                 self._last_ack_time=now
-            elif now-self._last_ack_time>1 and now-self._last_data_time<3:
+            elif now-self._last_ack_time>1 and now-self._last_data_time<2:
                 self.write_control(SYN_ACK,struct.pack("!QB",self._current_frame_id,0))
                 self._last_ack_time=now
         return super(Stream,self).loop()
