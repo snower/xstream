1d64eecf8b72e3a582510ff627d026665524aa5f
snower <sujin190@163.com>
2015-06-11 23:30:41 +0800

    fix

--- xstream/frame.py
+++ xstream/frame.py
@@ -33,7 +33,7 @@ class Frame(object):
         self.data = ''
 
 class StreamFrame(object):
-    FRAME_LEN = 1460 * 4 - 32
+    FRAME_LEN = 1460 * 4 - 64
 
     def __init__(self, stream_id, flag, action, data):
         self.stream_id = stream_id
