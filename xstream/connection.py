3e490c03c3213bed45c7f028cc2852a729bb6246
snower <sujian199@gmail.com>
2016-02-29 14:06:01 +0800

    update

--- xstream/connection.py
+++ xstream/connection.py
@@ -23,7 +23,10 @@ class Connection(EventEmitter):
         self._session = session
         self._crypto = connection.crypto
 
+        buf_size = int((connection._socket.getsockopt(socket.IPPROTO_TCP, socket.TCP_MAXSEG) or 1460) * 1.5 + 1)
         connection.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        connection.socket.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, buf_size)
+        connection.socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, buf_size)
         try:
             connection.socket.setsockopt(socket.SOL_SOCKET, socket.TCP_KEEPINTVL, 0)
         except: pass
