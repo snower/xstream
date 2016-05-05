5ff5a171364b1a1d15e40584f597841b89a1546a
snower <sujian199@gmail.com>
2016-05-05 21:44:38 +0800

    fix

--- xstream/center.py
+++ xstream/center.py
@@ -155,7 +155,7 @@ class Center(EventEmitter):
             bisect.insort_left(self.recv_frames, frame)
 
         if self.recv_frames and not self.ack_timeout_loop:
-            current().timeout(self.ttl * 0.7 / 1000, self.on_ack_timeout_loop, self.recv_index)
+            current().timeout(self.ttl * 1.5 / 1000, self.on_ack_timeout_loop, self.recv_index)
             self.ack_timeout_loop = True
 
     def on_drain(self, connection):
@@ -228,7 +228,7 @@ class Center(EventEmitter):
             data = struct.pack("!II", recv_index, self.recv_frames[0].index)
             self.write_action(ACTION_RESEND, data, index=0)
         if self.recv_frames and not self.closed:
-            current().timeout(self.ttl * 1.2 / 1000, self.on_ack_timeout_loop, self.recv_index)
+            current().timeout(self.ttl * 2 / 1000, self.on_ack_timeout_loop, self.recv_index)
         else:
             self.ack_timeout_loop = False
 
