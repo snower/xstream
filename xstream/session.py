b1f3ce8a828b0de42d2d31e3f6f1eabb9748b317
snower <sujian199@gmail.com>
2016-09-07 15:52:56 +0800

    fix

--- xstream/session.py
+++ xstream/session.py
@@ -113,7 +113,7 @@ class Session(EventEmitter):
         if callable(callback):
             callback(self, stream)
         if self._status == STATUS_CLOSED:
-            stream.do_close()
+            current().async(stream.do_close)
         return stream
 
     def close_stream(self, stream):
