2e7a9df042ece65ee7ec971dbfff7a8a1686b455
snower <sujian199@gmail.com>
2017-09-17 13:53:38 +0800

    fix

--- xstream/session.py
+++ xstream/session.py
@@ -341,4 +341,4 @@ class Session(EventEmitter):
         self.close()
 
     def __str__(self):
-        return "<%s %s,%s,%s>" % (super(Session, self).__str__(), self._session_id, len(self._connections), len(self._streams))
+        return "<%s %s,%s,%s,%s>" % (super(Session, self).__str__(), self._session_id, self._mss, len(self._connections), len(self._streams))
