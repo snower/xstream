8328f43062c715be62b7303ac01e95ce519fbd27
snower <sujian199@gmail.com>
2018-05-11 22:49:14 +0800

    fix

--- xstream/client.py
+++ xstream/client.py
@@ -130,10 +130,15 @@ class Client(EventEmitter):
             do_init_connection()
             self.init_connection_timeout = 0
             if not update_delay_rate:
-                self.init_connection_delay_rate = delay_rate
+                self.init_connection_delay_rate = delay_rate or 1
         elif len(self._connections) >= 1:
-            if delay_rate > self.init_connection_delay_rate:
-                delay_rate = self.init_connection_delay_rate
+            if delay_rate:
+                if delay_rate >= 1:
+                    self.init_connection_delay_rate = delay_rate
+                elif delay_rate > self.init_connection_delay_rate:
+                    delay_rate = self.init_connection_delay_rate
+            else:
+                delay_rate = 1
             timeout = time.time() + max(random.randint(90 * (len(self._connections) ** 2), 450 * (len(self._connections) ** 3)) * delay_rate, random.randint(2, 8))
             if self.init_connection_timeout == 0 or timeout < self.init_connection_timeout:
                 self.init_connection_timeout = timeout
