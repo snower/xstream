940c4759ba74264bc1b9dccae1df12178ae72792
snower <sujian199@gmail.com>
2020-02-12 12:06:50 +0800

    fix

--- xstream/frame.py
+++ xstream/frame.py
@@ -38,6 +38,24 @@ class Frame(object):
 
     def __cmp__(self, other):
         return cmp(self.index, other.index)
+    
+    def __eq__(self, other):
+        return self.index == other.index
+
+    def __gt__(self, other):
+        return self.index > other.index
+
+    def __lt__(self, other):
+        return self.index < other.index
+
+    def __ge__(self, other):
+        return self.index >= other.index
+
+    def __le__(self, other):
+        return self.index <= other.index
+
+    def __ne__(self, other):
+        return self.index != other.index
 
     def __len__(self):
         if self.data.__class__ == StreamFrame:
